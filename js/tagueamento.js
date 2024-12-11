b9b504a00ab4c1bd0c11f1ba2cc2f2bb94de4f37
Ismael Rojas <ismael711@gmail.com>
2024-12-11 14:56:59 -0300

    Conclusão do Teste Técnico

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -1,6 +1,85 @@
-// Preencha este arquivo com qualquer código que você necessite para realizar a
-// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
-// Manager. No último caso, não é necessário implementar a tag <noscript>.
-// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
-// para fazer a sua coleta.
-// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+// TESTE TÉCNICO - ISMAEL FERNANDES ROJAS
+document.addEventListener('DOMContentLoaded', function(){ 
+
+    const page_location = window.location.href
+
+    document.querySelector('a[href*="/contato"]').addEventListener('click', function(){
+        gtag('event', 'click', {
+           'page_location': page_location,
+           'element_name': 'entre_em_contato',
+           'element_group': 'menu'
+        });
+    });
+
+    document.querySelector('a[href*="autos_revista_vfinal"]').addEventListener('click', function () {
+        gtag('event', 'file_download', {
+            'page_location': page_location,
+            'element_name': 'download_pdf',
+            'element_group': 'menu'
+        });
+    });
+
+    if (window.location.href.includes('analise.html')) {
+
+        const cardTitle = document.querySelectorAll('.card-montadoras .card-title')
+
+        cardTitle.forEach(function (card) {
+            card.addEventListener('click', function () {
+               
+                gtag('event', 'click', {
+                    'page_location': page_location,
+                    'element_name': card.innerText,
+                    'element_group': 'ver_mais'
+                });
+            })
+        });
+       
+
+    }
+
+    if (window.location.href.includes('sobre.html')) {
+
+        const form = document.querySelector('form')
+        const form_id = form.getAttribute('id')
+        const form_name = form.getAttribute('name')
+        const form_destination = form.getAttribute('action')
+
+        form.addEventListener('focusin', function () {
+            gtag('event', 'form_start', {
+                'page_location': page_location,
+                'form_id': form_id,
+                'form_name': form_name,
+                'form_destination' : form_destination
+            });
+        });     
+
+        form.addEventListener('submit', function () { 
+            gtag('event', 'form_submit', {
+                'page_location': page_location,
+                'form_id': form_id,
+                'form_name': form_name,
+                'form_destination' : form_destination,
+                'form_submit_text': document.querySelector('[type="submit"]').innerText
+            });
+        });
+
+                     
+        (function formValidation(){
+        
+            if(document.body.innerText.includes('Obrigado pelo seu contato')){
+                               
+                gtag('event', 'view_form_success', {
+                    'page_location' : page_location,
+                    'form_id': form_id,
+                    'form_name': form_name
+                });
+        
+            } else {
+                setTimeout(formValidation, 1000);
+            }
+                })();
+        
+
+    }
+
+});
